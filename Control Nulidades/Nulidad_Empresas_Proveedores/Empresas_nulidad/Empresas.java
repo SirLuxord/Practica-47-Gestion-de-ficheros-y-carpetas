05380838e19c61b04ddbf58173db1c46ba657b36
SirLuxord <157701508+SirLuxord@users.noreply.github.com>
2024-05-02 21:28:42 +0100

    Update Empresas.java

--- Control Nulidades/Nulidad_Empresas_Proveedores/Empresas_nulidad/Empresas.java
+++ Control Nulidades/Nulidad_Empresas_Proveedores/Empresas_nulidad/Empresas.java
@@ -19,42 +19,7 @@ public class Empresas {
         this.TipoCarta = TipoCarta;
         this.Email = Email;
     }
-    /*
-    public void setCodigoEmpresa() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese el codigo de la empresa: ");
-        this.CodigoEmpresa = scanner.nextLine();
-    }
 
-
-    public void setNombreEmpresa(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese el nombre de la empresa: ");
-        this.NombreEmpresa = scanner.nextLine();
-    }
-
-    public void setNombreEmpleado(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese el nombre del empleado: ");
-        this.NombreEmpleado = scanner.nextLine();
-    }
-
-    public void setCargoEmpleado(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese el cargo del empleado: ");
-        this.CargoEmpleado = scanner.nextLine();
-    }
-
-    public void setTipoCarta(){
-        try{
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Ingrese el tipo de carta: ");
-            this.TipoCarta = scanner.nextLine();
-        } catch(InputMismatchException e){
-            System.out.println("Ingrese un numero entero entre 1 o 2");
-        }
-    }
-    */
     public String getCodigoEmpresa(){
         return this.CodigoEmpresa;
     }
